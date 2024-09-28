11890b5f03b93d750b9d2f59410e84c5debbcdec
Mina Shawky <150538677+Mina-Shawky@users.noreply.github.com>
2024-09-28 16:54:23 +0300

    Update Program.cs

--- Ticketz/Program.cs
+++ Ticketz/Program.cs
@@ -76,7 +76,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
+    pattern: "{controller=Movies}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
